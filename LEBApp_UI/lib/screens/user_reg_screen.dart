50908f9eb625f433a7e302d3784825504f4525c5
tozemaio <antonio.fmaio@gmail.com>
2021-05-18 00:32:52 +0100

    Login created page with backend connection

--- LEBApp_UI/lib/screens/user_reg_screen.dart
+++ LEBApp_UI/lib/screens/user_reg_screen.dart
@@ -1,11 +1,135 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
+import 'package:flutter/material.dart';
 import '../main.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:io';
+
 
 class User_Reg_Screen extends StatefulWidget {
   @override
   _User_Reg_ScreenState createState() => _User_Reg_ScreenState();
 }
+
+class _User_Reg_ScreenState extends State<User_Reg_Screen> {
+  TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
+
+  @override
+  Widget build(BuildContext context) {
+
+    final emailField = TextField(
+      obscureText: false,
+      style: style,
+      decoration: InputDecoration(hintText: 'Insert your email.'),
+
+    );
+
+    final passwordField = TextField(
+      obscureText: true,
+      style: style,
+      decoration: InputDecoration(hintText: 'Insert your password.'),
+    );
+
+    final loginButon = Material(
+      child: TextButton(
+        child: Text('Login'),
+        style: TextButton.styleFrom(
+          alignment: Alignment.bottomCenter,
+          primary: Colors.white,
+          backgroundColor: Colors.teal,
+          onSurface: Colors.grey,
+        ),
+        onPressed: () async {
+          print("Link to server...");
+
+          //var url = Uri.parse('http://192.168.1.110:8080/api/authenticate');
+          //var response = await http.post(url,headers: {HttpHeaders.CONTENT_TYPE: "application/json"}, body: {"username": "Admin", "password": "admin", "rememberMe": "false"});
+          //print('Response status: ${response.statusCode}');
+          //print('Response body: ${response.body}');
+
+          var  url = Uri.parse('http://192.168.1.110:8080/api/authenticate');
+          var body = json.encode({"username": "Admin", "password": "admin", "rememberMe": "false"});
+
+          Map<String,String> headers = {
+            'Content-type' : 'application/json',
+            'Accept': 'application/json',
+          };
+
+          final response = await http.post(url, body: body, headers: headers);
+          final responseJson = json.decode(response.body);
+          print(responseJson);
+          return response;
+
+        },
+      ),
+    );
+
+    final backButon = Material(
+      child: TextButton(
+        child: Text('BACK BUTTON'),
+        style: TextButton.styleFrom(
+          alignment: Alignment.bottomCenter,
+          primary: Colors.white,
+          backgroundColor: Colors.teal,
+          onSurface: Colors.grey,
+        ),
+        onPressed: () {
+          //button action
+          Navigator.of(context).push(MaterialPageRoute(builder: (context)=>MyApp()));
+        },
+      ),
+    );
+
+    return Scaffold(
+        appBar: AppBar(
+            title: const Text('Login Screen'),
+            backgroundColor: Colors.teal),
+        body: SingleChildScrollView(
+          child: Center(
+            child: Container(
+              color: Colors.white,
+              child: Padding(
+                padding: const EdgeInsets.all(36.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    SizedBox(
+                      height: 155.0,
+                      child: Image.asset(
+                        "assets/pp.jpg",
+                        fit: BoxFit.contain,
+                      ),
+                    ),
+                    SizedBox(height: 45.0),
+                    emailField,
+                    SizedBox(height: 25.0),
+                    passwordField,
+                    SizedBox(
+                      height: 35.0,
+                    ),
+                    loginButon,
+                    SizedBox(
+                      height: 15.0,
+                    ),
+                    backButon,
+                    SizedBox(
+                      height: 15.0,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        )
+    );
+  }
+}
+
+
+
+/*
 class _User_Reg_ScreenState extends State<User_Reg_Screen> {
   @override
   Widget build(BuildContext context) {
@@ -31,4 +155,4 @@ class _User_Reg_ScreenState extends State<User_Reg_Screen> {
     );
   }
 }
-
+ */
---
+++ LEBApp_UI/lib/widgets/UserLogin.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(UserLogin());
+
+class UserLogin extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Login'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
+
+  @override
+  Widget build(BuildContext context) {
+
+    final emailField = TextField(
+      obscureText: true,
+      style: style,
+      decoration: InputDecoration(hintText: 'Insert your email.'),
+
+    );
+
+    final passwordField = TextField(
+      obscureText: true,
+      style: style,
+      decoration: InputDecoration(hintText: 'Insert your password.'),
+    );
+
+    final loginButon = Material(
+      child: TextButton(
+        child: Text('Login'),
+        style: TextButton.styleFrom(
+          alignment: Alignment.bottomCenter,
+          primary: Colors.white,
+          backgroundColor: Colors.teal,
+          onSurface: Colors.grey,
+        ),
+        onPressed: () {
+          //button action
+        },
+      ),
+    );
+
+    return Scaffold(
+        body: SingleChildScrollView(
+          child: Center(
+            child: Container(
+              color: Colors.white,
+              child: Padding(
+                padding: const EdgeInsets.all(36.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    SizedBox(
+                      height: 155.0,
+                      child: Image.asset(
+                        "assets/hot.jpg",
+                        fit: BoxFit.contain,
+                      ),
+                    ),
+                    SizedBox(height: 45.0),
+                    emailField,
+                    SizedBox(height: 25.0),
+                    passwordField,
+                    SizedBox(
+                      height: 35.0,
+                    ),
+                    loginButon,
+                    SizedBox(
+                      height: 15.0,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        )
+    );
+  }
+}
