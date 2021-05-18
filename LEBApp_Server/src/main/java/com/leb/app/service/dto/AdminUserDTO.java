452fc9bc0f127af974ed4ef0b6b8f49f2cee6790
moscalcc <luismarques24@hotmail.com>
2021-05-19 00:12:47 +0100

    user registry

--- LEBApp_Server/src/main/java/com/leb/app/service/dto/AdminUserDTO.java
+++ LEBApp_Server/src/main/java/com/leb/app/service/dto/AdminUserDTO.java
@@ -4,6 +4,7 @@ import com.leb.app.config.Constants;
 import com.leb.app.domain.Authority;
 import com.leb.app.domain.User;
 import java.time.Instant;
+import java.time.LocalDate;
 import java.util.Set;
 import java.util.stream.Collectors;
 import javax.validation.constraints.*;
@@ -11,6 +12,7 @@ import javax.validation.constraints.*;
 /**
  * A DTO representing a user, with his authorities.
  */
+
 public class AdminUserDTO {
 
     private Long id;
@@ -45,6 +47,32 @@ public class AdminUserDTO {
     private String lastModifiedBy;
 
     private Instant lastModifiedDate;
+
+    private String phoneNumber;
+
+    private String nib;
+
+    private int nif;
+
+    private LocalDate birthday;
+
+    private String adress;
+
+    private boolean isTransporter;
+
+    private String favouriteTransport;
+
+    private boolean isProducer;
+
+    private String linkSocial;
+
+    private boolean isPoint;
+
+    private String openingTimePoint;
+
+    private boolean isDeliveryMan;
+
+    private String openingTime;
 
     private Set<String> authorities;
 
@@ -102,6 +130,110 @@ public class AdminUserDTO {
 
     public String getEmail() {
         return email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getNib() {
+        return nib;
+    }
+
+    public void setNib(String nib) {
+        this.nib = nib;
+    }
+
+    public int getNif() {
+        return nif;
+    }
+
+    public void setNif(int nif) {
+        this.nif = nif;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public boolean isTransporter() {
+        return isTransporter;
+    }
+
+    public void setTransporter(boolean isTransporter) {
+        this.isTransporter = isTransporter;
+    }
+
+    public String getFavouriteTransport() {
+        return favouriteTransport;
+    }
+
+    public void setFavouriteTransport(String favouriteTransport) {
+        this.favouriteTransport = favouriteTransport;
+    }
+
+    public boolean isProducer() {
+        return isProducer;
+    }
+
+    public void setProducer(boolean isProducer) {
+        this.isProducer = isProducer;
+    }
+
+    public String getLinkSocial() {
+        return linkSocial;
+    }
+
+    public void setLinkSocial(String linkSocial) {
+        this.linkSocial = linkSocial;
+    }
+
+    public boolean isPoint() {
+        return isPoint;
+    }
+
+    public void setPoint(boolean isPoint) {
+        this.isPoint = isPoint;
+    }
+
+    public String getOpeningTimePoint() {
+        return openingTimePoint;
+    }
+
+    public void setOpeningTimePoint(String openingTimePoint) {
+        this.openingTimePoint = openingTimePoint;
+    }
+
+    public boolean isDeliveryMan() {
+        return isDeliveryMan;
+    }
+
+    public void setDeliveryMan(boolean isDeliveryMan) {
+        this.isDeliveryMan = isDeliveryMan;
+    }
+
+    public String getOpeningTime() {
+        return openingTime;
+    }
+
+    public void setOpeningTime(String openingTime) {
+        this.openingTime = openingTime;
     }
 
     public void setEmail(String email) {
@@ -172,22 +304,16 @@ public class AdminUserDTO {
         this.authorities = authorities;
     }
 
-    // prettier-ignore
     @Override
     public String toString() {
-        return "AdminUserDTO{" +
-            "login='" + login + '\'' +
-            ", firstName='" + firstName + '\'' +
-            ", lastName='" + lastName + '\'' +
-            ", email='" + email + '\'' +
-            ", imageUrl='" + imageUrl + '\'' +
-            ", activated=" + activated +
-            ", langKey='" + langKey + '\'' +
-            ", createdBy=" + createdBy +
-            ", createdDate=" + createdDate +
-            ", lastModifiedBy='" + lastModifiedBy + '\'' +
-            ", lastModifiedDate=" + lastModifiedDate +
-            ", authorities=" + authorities +
-            "}";
+        return "AdminUserDTO [activated=" + activated + ", adress=" + adress + ", authorities=" + authorities
+                + ", birthday=" + birthday + ", createdBy=" + createdBy + ", createdDate=" + createdDate + ", email="
+                + email + ", favouriteTransport=" + favouriteTransport + ", firstName=" + firstName + ", id=" + id
+                + ", imageUrl=" + imageUrl + ", isDeliveryMan=" + isDeliveryMan + ", isPoint=" + isPoint
+                + ", isProducer=" + isProducer + ", isTransporter=" + isTransporter + ", langKey=" + langKey
+                + ", lastModifiedBy=" + lastModifiedBy + ", lastModifiedDate=" + lastModifiedDate + ", lastName="
+                + lastName + ", linkSocial=" + linkSocial + ", login=" + login + ", nib=" + nib + ", nif=" + nif
+                + ", openingTime=" + openingTime + ", openingTimePoint=" + openingTimePoint + ", phoneNumber="
+                + phoneNumber + "]";
     }
 }
